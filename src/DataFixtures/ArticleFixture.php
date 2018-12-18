c5f042c64ba4c82433db7f50a1a6a4f18e949df8
Mustiere Gabriel <mustiere.gabriel@gmail.com>
2018-12-18 16:29:31 +0100

    add author relation

--- src/DataFixtures/ArticleFixture.php
+++ src/DataFixtures/ArticleFixture.php
@@ -33,10 +33,7 @@ class ArticleFixture extends BaseFixture implements DependentFixtureInterface
      */
     protected function loadData(ObjectManager $manager)
     {
-        $this->createMany(10, 'main_articles', function ($count) use ($manager) {
-            $article = new Article();
-            $article->setTitle($this->faker->randomElement(self::$articleTitles))
-                ->setContent(<<<EOF
+        $content = <<<EOF
 Spicy **jalapeno bacon** ipsum dolor amet veniam shank in dolore. Ham hock nisi landjaeger cow,
 lorem proident [beef ribs](https://baconipsum.com/) aute enim veniam ut cillum pork chuck picanha. Dolore reprehenderit
 labore minim pork belly spare ribs cupim short loin in. Elit exercitation eiusmod dolore cow
@@ -53,18 +50,18 @@ mollit quis officia meatloaf tri-tip swine. Cow ut reprehenderit, buffalo incidi
 strip steak pork belly aliquip capicola officia. Labore deserunt esse chicken lorem shoulder tail consectetur
 cow est ribeye adipisicing. Pig hamburger pork belly enim. Do porchetta minim capicola irure pancetta chuck
 fugiat.
-EOF
-            );
+EOF;
+        $this->createMany(10, 'main_articles', function ($count) use ($manager, $content) {
+            $article = new Article();
+            $article->setTitle($this->faker->randomElement(self::$articleTitles))
+                ->setContent($content)->setHeartCount($this->faker->numberBetween(5, 100))
+                ->setImageFilename($this->faker->randomElement(self::$articleImages))
+                ->setAuthor($this->getRandomReference('main_users'));
 
             // publish most articles
             if ($this->faker->boolean(70)) {
                 $article->setPublishedAt($this->faker->dateTimeBetween('-100 days', '-1 days'));
             }
-
-            $article->setAuthor($this->faker->randomElement(self::$articleAuthors))
-                ->setHeartCount($this->faker->numberBetween(5, 100))
-                ->setImageFilename($this->faker->randomElement(self::$articleImages))
-            ;
 
             $tags = $this->getRandomReferences('main_tags', $this->faker->numberBetween(0, 5));
             foreach ($tags as $tag) {
@@ -84,6 +81,7 @@ EOF
     {
         return [
             TagFixture::class,
+            UserFixture::class,
         ];
     }
 }
--- src/Entity/Article.php
+++ src/Entity/Article.php
@@ -45,11 +45,6 @@ class Article
     private $publishedAt;
 
     /**
-     * @ORM\Column(type="string", length=255)
-     */
-    private $author;
-
-    /**
      * @ORM\Column(type="integer")
      */
     private $heartCount = 0;
@@ -69,6 +64,12 @@ class Article
      * @ORM\ManyToMany(targetEntity="App\Entity\Tag", inversedBy="articles")
      */
     private $tags;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\User", inversedBy="articles")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $author;
 
     /**
      * Article constructor.
@@ -163,26 +164,6 @@ class Article
     public function setPublishedAt(\DateTimeInterface $publishedAt): self
     {
         $this->publishedAt = $publishedAt;
-
-        return $this;
-    }
-
-    /**
-     * @return null|string
-     */
-    public function getAuthor(): ?string
-    {
-        return $this->author;
-    }
-
-    /**
-     * @param string $author
-     *
-     * @return Article
-     */
-    public function setAuthor(string $author): self
-    {
-        $this->author = $author;
 
         return $this;
     }
@@ -328,6 +309,18 @@ class Article
         if ($this->tags->contains($tag)) {
             $this->tags->removeElement($tag);
         }
+
+        return $this;
+    }
+
+    public function getAuthor(): ?User
+    {
+        return $this->author;
+    }
+
+    public function setAuthor(?User $author): self
+    {
+        $this->author = $author;
 
         return $this;
     }
