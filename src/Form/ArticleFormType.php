5422d52faf38e1ab3f62e4d9d1ab7b56b80515c9
Mustiere Gabriel <mustiere.gabriel@gmail.com>
2019-01-01 10:42:38 +0100

    author entity type

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\User;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
@@ -29,6 +31,13 @@ class ArticleFormType extends AbstractType
             ])
             ->add('publishedAt', null, [
                 'widget' => 'single_text',
+            ])
+            ->add('author', EntityType::class, [
+                'class' => User::class,
+                'placeholder' => 'Choose an author',
+                'choice_label' => function (User $user) {
+                    return sprintf('(%d) %s', $user->getId(), $user->getEmail());
+                },
             ]);
     }
 
