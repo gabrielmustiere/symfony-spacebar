8a9ae5f68ab3964f7e7dada9f20ac53039704c2a
Mustiere Gabriel <mustiere.gabriel@gmail.com>
2019-01-01 11:43:06 +0100

    add invalid message

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -51,6 +51,7 @@ class ArticleFormType extends AbstractType
                 'class' => User::class,
                 'placeholder' => 'Choose an author',
                 'choices' => $this->userRepository->findAllEmailAlphabetical(),
+                'invalid_message' => 'Symfony is too smart for your hacking',
                 'choice_label' => function (User $user) {
                     return sprintf('(%d) %s', $user->getId(), $user->getEmail());
                 },
